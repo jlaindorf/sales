e4fed28163a6d82fbfbaa5a791b1b4d75c0d6c5f
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-26 23:58:16 -0300

    feat: add config

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set('America/Sao_Paulo');
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, X-Requested-With");
