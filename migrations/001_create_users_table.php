91c6703faf70ddb626e256f57b6bb493418a7ada
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-27 20:13:19 -0300

    fix: include created and updated at to users table

--- migrations/001_create_users_table.php
+++ migrations/001_create_users_table.php
@@ -12,7 +12,9 @@ try {
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             email VARCHAR(255) NOT NULL UNIQUE,
-            password VARCHAR(255) NOT NULL
+            password VARCHAR(255) NOT NULL,
+            created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+            updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
         )
     ";
 
