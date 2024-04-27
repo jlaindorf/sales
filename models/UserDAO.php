70e9e0a58cfe344ccf635f8a0501e55d2ff90bb3
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-27 19:57:48 -0300

    refact: erase duplicated data inutilized

--- models/UserDAO.php
+++ models/UserDAO.php
@@ -3,43 +3,30 @@ class UserDAO
 {
     private $connection;
 
-    public function __construct(PDO $connection)
+    public function __construct()
     {
-        $this->connection = $connection;
-    }
-
-    public function createTableIfNotExists()
-    {
-        $sql = "
-            CREATE TABLE IF NOT EXISTS users (
-                id SERIAL PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                email VARCHAR(255) NOT NULL,
-                password VARCHAR(255) NOT NULL
-            )
-        ";
-
-        try {
-            $this->connection->exec($sql);
-            echo "Tabela 'users' criada com sucesso!";
-        } catch (PDOException $e) {
-            die("Erro ao criar tabela 'users': " . $e->getMessage());
-        }
+        $this->connection = new PDO("pgsql:host=localhost;dbname=api_sales", "admin", "admin");;
     }
 
     public function insert(User $user)
     {
-        $sql = "INSERT INTO users (name, email, password)
-                VALUES (:name, :email, :password)";
+        try {
+            $sql = "INSERT INTO users (name, email, password)
+                VALUES (:name_value, :email_value, :password_value)";
 
-        $statement = $this->connection->prepare($sql);
-        $statement->execute([
-            'name' => $user->getName(),
-            'email' => $user->getEmail(),
-            'password' => $user->getPassword()
-        ]);
+            $statement = $this->connection->prepare($sql);
 
-        return $this->connection->lastInsertId();
+            $statement->bindValue(":name_value", $user->getName());
+            $statement->bindValue(":email_value", $user->getEmail());
+            $statement->bindValue(":password_value", $user->getPassword());
+
+            $statement->execute();
+
+            return ['success' => true];
+        } catch (PDOException $error) {
+            debug($error->getMessage());
+            return ['success' => false];
+        }
     }
 
     public function findOneById($id)
@@ -50,6 +37,66 @@ class UserDAO
 
         return $statement->fetch(PDO::FETCH_ASSOC);
     }
+    public function findMany()
+    {
+        $sql = "SELECT id,name from users";
 
-    
+        $statement = ($this->getConnection())->prepare($sql);
+        $statement->execute();
+
+        return $statement->fetchAll(PDO::FETCH_ASSOC);
+    }
+    public function findOne($id)
+    {
+        $sql = "SELECT * FROM users WHERE id = :id_value";
+
+        $statement = $this->connection->prepare($sql);
+        $statement->bindValue(":id_value", $id);
+        $statement->execute();
+
+        return $statement->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function updateOne($id, $data)
+    {
+        $userInDatabase = $this->findOne($id);
+
+        $sql = "UPDATE users 
+                SET name = :name_value,
+                    email = :email_value,
+                    password = :password_value
+                WHERE id = :id_value";
+
+        $statement = $this->connection->prepare($sql);
+
+        $statement->bindValue(":id_value", $id);
+        $statement->bindValue(":name_value", $data->name ?? $userInDatabase['name']);
+        $statement->bindValue(":email_value", $data->email ?? $userInDatabase['email']);
+        $statement->bindValue(":password_value", $data->password ?? $userInDatabase['password']);
+
+        $statement->execute();
+
+        return ['success' => true];
+    }
+
+    public function deleteOne($id)
+    {
+        try {
+            $sql = "DELETE FROM users WHERE id = :id_value";
+
+            $statement = $this->connection->prepare($sql);
+            $statement->bindValue(":id_value", $id);
+            $statement->execute();
+
+            return ['success' => true];
+        } catch (PDOException $error) {
+            debug($error->getMessage());
+            return ['success' => false];
+        }
+    }
+
+    public function getConnection()
+    {
+        return $this->connection;
+    }
 }
