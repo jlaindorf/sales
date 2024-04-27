067752d7f32d9f261a01f56b7a1a85c8f46a39e6
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-27 19:24:38 -0300

    refact: fix user email to not null

--- migrations/001_create_users_table.php
+++ migrations/001_create_users_table.php
@@ -11,7 +11,7 @@ try {
         CREATE TABLE IF NOT EXISTS users (
             id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
-            email VARCHAR(255) NOT NULL,
+            email VARCHAR(255) NOT NULL UNIQUE,
             password VARCHAR(255) NOT NULL
         )
     ";
