0d88a3d25f52e7c6cbddb047bc5282d182645abe
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-27 00:02:20 -0300

    feat: add clients router

---
+++ routes/clients.php
@@ -0,0 +1,19 @@
+<?php
+require_once '../config.php';
+require_once '../controllers/ClientController.php';
+
+$method = $_SERVER['REQUEST_METHOD'];
+$controller = new ClientController();
+
+if ($method === 'POST') {
+    $controller->createOne();
+    
+} else if($method === 'GET' && $_GET['id']) {
+    $controller->listOne();
+}
+else if ($method === 'DELETE') {
+    $controller->deleteOne();
+}
+else if ($method === 'PUT') {
+    $controller->updateOne();
+}
