473df324bd8135af60ea1a41bab89de2cf3f3884
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-04-26 23:59:02 -0300

    feat: add utils file

---
+++ utils.php
@@ -0,0 +1,41 @@
+<?php
+function getBody() {
+    return json_decode(file_get_contents("php://input"));
+}
+function readFileContent($fileName){
+    return json_decode(file_get_contents($fileName));
+ }
+ function saveFileContent($fileName, $content) {
+    file_put_contents($fileName, json_encode($content), JSON_PRETTY_PRINT);
+  }
+
+function sanitizeString($value){
+    return filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
+}
+
+function responseError($message, $status){
+    http_response_code($status);
+    echo json_encode(['error' => $message]);
+    exit;
+}
+function response($data, $status){
+    http_response_code($status);
+    echo json_encode($data);
+    exit;
+}
+function debug($content)
+{
+  echo '<pre>';
+  echo var_dump($content);
+  echo '</pre>';
+}
+function sanitizeInput($data, $property, $filterType, $isObject = true) {
+    if($isObject) {
+      return isset($data->$property) ? filter_var($data->$property, $filterType) : null;
+    } else {
+      return isset($data[$property]) ? filter_var($data[$property], $filterType) : null;
+    }
+   
+  }
+  
+  
