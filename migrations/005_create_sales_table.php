b8abd0d7ec061b29f49438f506959cf098eca976
Julio Laindorf <125938958+jlaindorf@users.noreply.github.com>
2024-05-01 14:47:38 -0300

    feat: create sales table

--- migrations/005_create_sales_table.php
+++ migrations/005_create_sales_table.php
@@ -11,15 +11,13 @@ try {
     $sql="
         CREATE TABLE IF NOT EXISTS sales (
         id SERIAL PRIMARY KEY,
-        user_id INT NOT NULL,
         client_id INT NOT NULL,
         product_id INT NOT NULL,
         quant INT NOT NULL,
-        total_price NUMERIC(10, 2) NOT NULL,
+        total_price NUMERIC(10, 2),
         payment_id INT NOT NULL,
         created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
         updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-        FOREIGN KEY (user_id) REFERENCES users(id),
         FOREIGN KEY (client_id) REFERENCES clients(id),
         FOREIGN KEY (product_id) REFERENCES products(id),
         FOREIGN KEY (payment_id) REFERENCES payments(id)
